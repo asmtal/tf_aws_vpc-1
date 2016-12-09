c984164fceee22720297db7fb688c230fd0d099b
Kevin Pedersen <byte_count@hotmail.com>
2016-12-09 13:25:33 -0500

    adding database subnet group

---
+++ database.tf
@@ -0,0 +1,13 @@
+resource "aws_db_subnet_group" "tf_rds_sg" {
+  name       = "tf_cdh_rds_sg"
+  subnet_ids = ["${element(module.vpc.private_subnets, 0)}", "${element(module.vpc.private_subnets, 1)}"]
+
+  tags {
+    Name = "CDH DB subnet group"
+  }
+}
+
+output "cmdatabase_subnet" {
+  value = "${aws_db_subnet_group.tf_rds_sg.id}"
+}
+
