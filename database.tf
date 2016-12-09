809314eb3b43aaa5cbae6830ea3426f4c25d40a5
Kevin Pedersen <byte_count@hotmail.com>
2016-12-09 13:28:06 -0500

    correcting rds subnet group name

--- database.tf
+++ database.tf
@@ -1,5 +1,5 @@
 resource "aws_db_subnet_group" "tf_rds_sg" {
-  name       = "tf_cdh_rds_sg"
+  name       = "tf_rds_sg"
   subnet_ids = ["${element(module.vpc.private_subnets, 0)}", "${element(module.vpc.private_subnets, 1)}"]
 
   tags {
